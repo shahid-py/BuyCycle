b8c834dbc9ad0dc7bc8fcd91dca74b51689b6a0d
Mohd Shahid Khan <khan42290@gmail.com>
2022-06-01 12:20:53 +0530

    chore:installation of packages

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const API_URL = "https://BuyCycle-backend.shahid-v2.repl.co";
+export const RAZORPAY_CHECKOUT_URL = "https://checkout.razorpay.com/v1/checkout.js"
+export const RAZORPAY_KEY = "rzp_test_i4ZygPH7SECPqU";
+export const RAZORPAY_LOGO = "https://razorpay.com/assets/razorpay-glyph.svg"
